901d4edafb118ccdb60597f999a4d2369a8d3f88
Dave <bordoley@gmail.com>
2015-01-18 14:08:06 -0800

    Add a test project.

---
+++ Sparse.Tests/PrimitiveTests.fs
@@ -0,0 +1,15 @@
+﻿namespace Sparse.Tests
+
+open NUnit.Framework
+open FsUnit
+open Sparse
+
+module PrimitiveTests =
+    [<Test>]
+    let ``test pzero`` () =
+        let input = CharStream.Create "test"
+        match pzero input with
+        | Fail i -> i |> should equal 0
+        | Success _ -> failwith "expected parsing to fail."
+       
+
